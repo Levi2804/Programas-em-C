7a432694e2c5039f835f85a30b1c3cd66229760b
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 10:27:20 -0300

    Update calculadora.c
    Na opção 4 (divisão), incluí um if-else para lidar com a situação em que o denominador é 0, notificando o usuário de que a divisão é indefinida.

--- calculadora.c
+++ calculadora.c
@@ -35,7 +35,12 @@ int main ()
 			printf("%.2f * %.2f = %.2f", num1, num2, num1 * num2);
 			break;
 		case 4:
-			printf("%.2f / %.2f = %.2f", num1, num2, num1 / num2);
+			//Aqui, o if-else vai verificar se o denominador é zero. Se sim, a divisão é indefinida.
+			if(num2 == 0){
+				printf("\nA divisão é indefinida!");
+			} else{
+				printf("%.2f / %.2f = %.2f", num1, num2, num1 / num2);
+			}
 			break;
 		default:
 			printf("Essa operação não pode ser realizada.");
